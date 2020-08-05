8ffd7320ebea18b12b630b41aa024478cfb1ca27
Jack Yang <h36yang@gmail.com>
2020-08-04 21:06:52 -0400

    Added code for find/replace strings

---
+++ strings/04_find_replace/find.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	sampleString := "I really enjoy the Go language. It's one of my favourites"
+
+	// Find String Contains
+	searchTerm := "Go"
+	result := strings.Contains(sampleString, searchTerm)
+	fmt.Printf("The sample text includes %q: %t\n", searchTerm, result)
+
+	// Find String Starts With
+	searchTerm2 := "I"
+	result2 := strings.HasPrefix(sampleString, searchTerm2)
+	fmt.Printf("The sample text starts with %q: %t\n", searchTerm2, result2)
+
+	// Find String Ends With
+	searchTerm3 := "favourites"
+	result3 := strings.HasSuffix(sampleString, searchTerm3)
+	fmt.Printf("The sample text ends with %q: %t\n", searchTerm3, result3)
+
+	// Replace String
+	sampleString2 := "This is my string. There are many strings like it, but this one is mine."
+	sampleString2 = strings.Replace(sampleString2, "string", "compiler", -1)
+	fmt.Println(sampleString2)
+}
